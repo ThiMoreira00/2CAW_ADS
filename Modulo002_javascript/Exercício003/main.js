77bf6f9a0a881f2fc56b5ad8ba70701b5286017d
ThiMoreira00 <thiago.moreira1@outlook.pt>
2023-05-10 09:30:19 -0300

    Create Exercício003

---
+++ Modulo002_javascript/Exercício003/main.js
@@ -0,0 +1,11 @@
+{
+
+    let lugar;
+    lugar = document.getElementById('idLugar');
+    lugar.innerHTML = "<p><h1>Olá, mundo!</h1></p>";
+    lugar.style.color = "white";
+    document.body.style.backgroundColor = "#013368";
+    document.getElementById("myImg").src = "https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif?resize=476%2C280&ssl=1";
+
+
+}
