df338edaf26cc2cfe804d12bcfa2a01a89bf0491
Thiago M <129688942+ThiMoreira00@users.noreply.github.com>
2023-05-29 12:03:28 -0300

    Update main.js

--- Codinng/src/main.js
+++ Codinng/src/main.js
@@ -1,6 +1,12 @@
-preview = document.getElementById('preview');
+preview = document.getElementById('preview-section');
 editor = document.getElementById('editor');
 
 function promptProgramar() {
+    preview.style = '';
+    preview.style.backgroundColor = 'white';
+    preview.style.color = 'black';
+    
+
     preview.innerHTML = editor.value;
+    preview.style.all = 'unset';
 }
